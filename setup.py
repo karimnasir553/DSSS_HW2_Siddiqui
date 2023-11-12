4cf1d09f3047036faf2057364809c08112f2dfb6
karimnasir553 <karimnasir97@gmail.com>
2023-11-12 19:40:58 +0100

    task5_changes2

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='math_quiz_game',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        
-        # Add any other Python packages your project depends on
-    ],
-    entry_points={
-        'console_scripts': [
-            'math-quiz=math_quiz.quiz:math_quiz',
-        ],
-    },
-)
+setup()
