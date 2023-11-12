5a43512ff4d1df3553e2a0f7dab2d4bacbede537
karimnasir553 <karimnasir97@gmail.com>
2023-11-12 19:49:01 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz_game',
+    name='math-quiz-game',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        
-        # Add any other Python packages your project depends on
+        # Add any dependencies here
     ],
     entry_points={
         'console_scripts': [
